8488fd75b98bd53ae12cf5e1866a747a901f6d35
twwhrse <136729833+twwhrse@users.noreply.github.com>
2024-12-17 22:47:24 +0300

    code

--- 10-lab-mispis-main/AdministrativeEmployee.java
+++ 10-lab-mispis-main/AdministrativeEmployee.java
@@ -19,5 +19,6 @@ public class AdministrativeEmployee extends Employee {
         return "AdministrativeEmployee{" +
                 "name='" + name + '\'' +
                 '}';
+        
     }
 }
